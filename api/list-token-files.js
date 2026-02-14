b6189fb9065aad2722bda7d78a3a7dcfd36aa50c
namviat <namviat06@gmail.com>
2026-02-14 17:53:47 +0530

    Update list-token-files.js

--- api/list-token-files.js
+++ api/list-token-files.js
@@ -9,7 +9,7 @@ module.exports = async (req, res) => {
   cloudinary.config({
      cloud_name: process.env.CLOUDINARY_CLOUD_NAME ,
     api_key: process.env.CLOUDINARY_API_KEY ,
-    api_secret: process.env.CLOUDINARY_API_SECRET , 
+    api_secret: process.env.CLOUDINARY_API_SECRET 
   });
 
   try {
